c94a3bdf552d8759baed3c79d59458bf62af9e41
MOSTAFA <mhrmo75@gmail.com>
2025-07-07 09:17:42 +0330

    default project

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import React, { useState } from 'react'
-import Blog from './Components/Blog'
-import Login from './Components/Login'
+import React, { useState } from "react";
+import Blog from "./Components/Blog";
+import Login from "./Components/Login";
 
 export default function App() {
-  const [token,setToken]=useState()
-  const handleToken=(token)=>{
-    setToken(token)
+  const [token, setToken] = useState();
+  const handleToken = (token) => {
+    setToken(token);
+  };
+  const handleLogout=()=>{
+    setToken(undefined)
   }
-  return (
-    <>
-      {token?<Blog/>:<Login handleToken={handleToken}/>}
-    </>
-  )
+  return <>{token ? <Blog handleLogout={handleLogout}/> : <Login handleToken={handleToken} />}</>;
 }
