ec897457282aee779f00a715b20f005ca935ffb8
MOSTAFA <mhrmo75@gmail.com>
2025-07-20 12:00:59 +0330

    hook forms

---
+++ src/Hooks/useFormFields.js
@@ -0,0 +1,10 @@
+import { useState } from "react"
+
+const useFormFields=(initialState={})=>{
+    const [fields,setFields]=useState(initialState)
+    const handleChange=(e)=>{
+    setFields({...fields,[e.target.name]:e.target.value})
+    }
+    return [fields,handleChange]
+}
+export default useFormFields
